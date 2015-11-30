052e72a3a58a3c42d4149ccc74f5a7463fe75ad6
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-12-01 03:34:09 +0800

    Typography Sass done

--- custom_functions.php
+++ custom_functions.php
@@ -2,3 +2,9 @@
 
 //set the post thumbnail default size to siut the theme layout
 set_post_thumbnail_size(811, 456, true);
+
+//* Enqueue Google Font Roboto
+add_action( 'wp_enqueue_scripts', 'enqueue_google_font_Roboto');
+function enqueue_google_font_Roboto() {	
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,500');
+}
