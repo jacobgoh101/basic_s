727d268c000495afa81c7400c9fc853804104dda
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-12-01 01:57:31 +0800

    Added Grunt for auto processing Sass

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options:{
+          style:'compressed'
+        },
+        files: {
+          'style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    postcss: {
+      options: {
+      map: true, // inline sourcemaps      
+
+      processors: [
+        require('pixrem')(), // add fallbacks for rem units
+        require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+        require('cssnano')() // minify the result
+        ]
+      },
+      files: {
+        'style.css' : 'style.css'
+      }
+    },
+    watch: {
+      set1: {
+        files: 'sass/*.scss',
+        tasks: ['sass', 'postcss']
+      },
+      set2: {
+        files: 'sass/**/*.scss',
+        tasks: ['sass', 'postcss']
+      },
+      set3: {
+        files: 'sass/**/**/*.scss',
+        tasks: ['sass', 'postcss']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.registerTask('default',['watch']);
+}
